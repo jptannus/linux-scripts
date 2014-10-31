ecbfa5b1796ac68129be1531a9f7fc347a9fc77a
Joao Paulo <jptannus@gmail.com>
2014-10-31 01:36:12 -0200

    Adding node installation script

---
+++ node-install.sh
@@ -0,0 +1,13 @@
+# Get every dependency first
+sudo apt-get install build-essential
+sudo apt-get install curl
+# Do the magic
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+. ~/.bashrc
+mkdir ~/local
+mkdir ~/node-latest-install
+cd ~/node-latest-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=~/local
+make install
+curl https://www.npmjs.org/install.sh | sh
