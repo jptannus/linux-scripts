a462dc9274aabca70ff16f910cd121d523a18ba6
jptannus <jptannus@gmail.com>
2015-09-12 13:50:09 -0300

    Removing npm step
    NPM now comes with Node.

--- node-install.sh
+++ node-install.sh
@@ -10,4 +10,4 @@ cd ~/node-latest-install
 curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
 ./configure --prefix=~/local
 make install
-curl https://www.npmjs.org/install.sh | sh
+# Be happy :) 
